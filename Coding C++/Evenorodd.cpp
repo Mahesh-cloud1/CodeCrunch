0602a4e56ff3479c8e431314bf59a6afd1627b63
Mahesh-cloud1 <themahesha15@gmail.com>
2024-07-03 02:48:56 -0700

    Add files via upload

---
+++ Coding C++/Evenorodd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"Enter any number: ";
+    cin>>a;
+
+    if (a%2==0)
+    cout<<a<<" is Even number";
+    else
+    cout<<a<<" is Odd number";
+    return 0;
+
+}
