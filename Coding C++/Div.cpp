8b4e4bdef38c61057f6b1b49044952f106bd2c4c
Mahesh-cloud1 <themahesha15@gmail.com>
2024-07-03 02:12:57 -0700

    Add files via upload

---
+++ Coding C++/Div.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    cout<<"Enter any two numbers: ";
+    cin>>a>>b;
+
+    int quotient;
+    quotient=a/b;
+    cout<<"Answer of "<<a<<"/"<<b<<" is: "<<quotient;
+    return 0;
+
+}
