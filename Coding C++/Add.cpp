4ddbd4be6b09be447dcf9bc2e0642e717b33c402
Mahesh-cloud1 <themahesha15@gmail.com>
2024-07-03 01:55:08 -0700

    Add files via upload

---
+++ Coding C++/Add.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    cout<<"Enter any two numbers: ";
+    cin>>a>>b;
+
+    int sum;
+    sum=a+b;
+    cout<<"Sum of "<<a<<" and "<<b<<" is: "<<sum;
+
+    return 0;
+    
+}
