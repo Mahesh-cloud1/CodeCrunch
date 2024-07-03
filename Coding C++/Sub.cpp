013efb8bc5c7741ef84260d42126e8cca1a457ee
Mahesh-cloud1 <themahesha15@gmail.com>
2024-07-03 01:57:24 -0700

    Add files via upload

---
+++ Coding C++/Sub.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    cout<<"Enter any two numbers: ";
+    cin>>a>>b;
+
+    int diff;
+    diff=a-b;
+    cout<<"Difference of "<<a<<" and "<<b<<" is: "<<diff;
+    return 0;
+
+}
