1f7596a571395fd17e97f2fc578a7681efc20e5f
Mahesh-cloud1 <themahesha15@gmail.com>
2024-07-03 02:10:26 -0700

    Add files via upload

---
+++ Coding C++/Mul.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    cout<<"Enter any two numbers: ";
+    cin>>a>>b;
+
+    int product;
+    product=a*b;
+    cout<<"Product of "<<a<<" and "<<b<<" is: "<<product;
+    return 0;
+    
+}
