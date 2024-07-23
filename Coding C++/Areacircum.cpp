24b808044f8d5ebd32e6724db8bd481856d1d338
Mahesh-cloud1 <themahesha15@gmail.com>
2024-07-23 08:44:12 -0700

    Add files via upload

---
+++ Coding C++/Areacircum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int r;
+    cout<<"Enter the radius of circle: ";
+    cin>>r;
+
+    cout<<"Cicumference of Circle = "<<2*3.14*r<<endl;
+    cout<<"Area of circle = "<<3.14*r*r;
+
+    return 0;
+}
